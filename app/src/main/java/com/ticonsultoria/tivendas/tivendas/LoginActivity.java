d2e4d9b042c0cf9465e08d765fb26f8aa003b778
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2018-01-16 11:59:02 -0200

    Tela Usuarios Login como primeira tela link loginActivity para mainActivity

--- app/src/main/java/com/ticonsultoria/tivendas/tivendas/LoginActivity.java
+++ app/src/main/java/com/ticonsultoria/tivendas/tivendas/LoginActivity.java
@@ -1,13 +1,35 @@
 package com.ticonsultoria.tivendas.tivendas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtLogin;
+    EditText edtSenha;
+    Button btnLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtLogin = findViewById(R.id.edt_login_login);
+        edtSenha = findViewById(R.id.edt_login_senha);
+        btnLogar = findViewById(R.id.btn_login);
+
+
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
