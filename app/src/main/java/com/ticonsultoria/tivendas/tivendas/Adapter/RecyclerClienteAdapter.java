9e7a0cd383ee18abd9d96c41b31d52d3a170f598
Helder Paiva <helder.ead@gmail.com>
2018-01-18 20:47:33 -0300

    recycler view criado mas nao esta mostrando clientes cadastrados

---
+++ app/src/main/java/com/ticonsultoria/tivendas/tivendas/Adapter/RecyclerClienteAdapter.java
@@ -0,0 +1,157 @@
+package com.ticonsultoria.tivendas.tivendas.Adapter;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ticonsultoria.tivendas.tivendas.R;
+import com.ticonsultoria.tivendas.tivendas.model.Cliente;
+import com.ticonsultoria.tivendas.tivendas.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Helder on 18/01/2018.
+ */
+
+public class RecyclerClienteAdapter extends RecyclerView.Adapter<RecyclerClienteAdapter.RecyclerHolder>{
+
+    private final List<Cliente> mClientes;
+    private Context context;
+
+    public RecyclerClienteAdapter(ArrayList clientes, Context c) {
+        mClientes = clientes;
+        context = c;
+    }
+    @Override
+    public RecyclerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new RecyclerClienteAdapter.RecyclerHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_view_clientes, parent, false));
+    }
+    @Override
+    public void onBindViewHolder(RecyclerHolder holder,final int position) {
+
+        holder.title.setText(mClientes.get(position).getNome());
+        holder.title.setText(mClientes.get(position).getNomeMercado());
+
+        holder.editButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateItem(position);
+            }
+
+        });
+
+        holder.deleteButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                removerItem(position);
+            }
+        });
+    }
+    public int getItemCont(){
+        return mClientes != null ? mClientes.size() : 0;
+    }
+    public void updateList(Cliente cliente){ insertItem(cliente);}
+
+    private void insertItem(Cliente cliente){
+        mClientes.add(cliente);
+        notifyItemInserted(getItemCont());
+    }
+
+    private void updateItem(int position) {
+
+        final int p = position;
+        Cliente user = mClientes.get(position);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        final View dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_usuario, null);
+
+        final EditText edtDialogNome = dialogView.findViewById(R.id.edt_dialog_cliente_nome);
+        final EditText edtDialogMercado = dialogView.findViewById(R.id.edt_dialog_cliente_mercado);
+
+        edtDialogNome.setText(user.getNome());
+        edtDialogMercado.setText(user.getNomeMercado());
+
+
+        builder.setView(dialogView).setTitle("Editar cliente")
+                .setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Cliente cliente = new Cliente();
+
+                        cliente.setNome(edtDialogNome.getText().toString());
+                        cliente.setNomeMercado(edtDialogMercado.getText().toString());
+
+                        //Verificar se os campos estão preenchidos
+                        if (!cliente.getNome().equals("") && !cliente.getNomeMercado().equals("")) {
+
+                            mClientes.set(p, cliente);
+                            notifyItemChanged(p);
+
+                        } else { //Campos não preenchidos
+                            Toast.makeText(context,
+                                    "Preencha todos os campos para salvar",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancelar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+    private void removerItem(int position) {
+
+        final int p = position;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setTitle("Excluir cliente").setMessage("Tem certeza que deseja excluir o cliente " + mClientes.get(p).getNome() + "?")
+                .setPositiveButton("Excluir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        mClientes.remove(p);
+                        notifyItemRemoved(p);
+                        notifyItemRangeChanged(p, mClientes.size());
+                    }
+                })
+                .setNegativeButton("Cancelar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    class RecyclerHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView nivel;
+        public ImageButton editButton;
+        public ImageButton deleteButton;
+
+        public RecyclerHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.txt_line_layout_name);
+            nivel = itemView.findViewById(R.id.txt_line_layout_nome_mercado);
+            editButton = itemView.findViewById(R.id.btn_line_layout_edit_cliente);
+            deleteButton = itemView.findViewById(R.id.btn_line_layout_delete_cliente);
+        }
+    }
+}
--- app/src/main/java/com/ticonsultoria/tivendas/tivendas/ClientesFragment.java
+++ app/src/main/java/com/ticonsultoria/tivendas/tivendas/ClientesFragment.java
@@ -1,23 +1,108 @@
 package com.ticonsultoria.tivendas.tivendas;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.ticonsultoria.tivendas.tivendas.Adapter.RecyclerClienteAdapter;
+import com.ticonsultoria.tivendas.tivendas.Adapter.RecyclerUsuariosAdapter;
+import com.ticonsultoria.tivendas.tivendas.model.Cliente;
+import com.ticonsultoria.tivendas.tivendas.model.Usuario;
+
+import java.util.ArrayList;
 
 
 public class ClientesFragment extends Fragment {
 
-    public ClientesFragment() {
+    RecyclerView mRecyclerView;
+    FloatingActionButton floatingActionButton;
 
+    private RecyclerClienteAdapter mAdapter;
+    public ClientesFragment() {
+    //precisa de um construtor limpo
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clientes, container, false);
+        View view = inflater.inflate(R.layout.fragment_clientes, container, false);
+
+        mRecyclerView = view.findViewById(R.id.rv_clientes);
+        floatingActionButton = view.findViewById(R.id.fab_clientes_add);
+        setupRecycler();
+
+        //Acão do botão flutuante de adicionar
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                final View dialogView = getActivity().getLayoutInflater().inflate(R.layout.dialog_cliente, null);
+
+
+                builder.setView(dialogView).setTitle("Adicionar Cliente")
+                        .setPositiveButton("Adicionar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                Cliente cliente = new Cliente();
+
+                                final EditText edtDialogNome = dialogView.findViewById(R.id.edt_dialog_cliente_nome);
+                                final EditText edtDialogMercado = dialogView.findViewById(R.id.edt_dialog_cliente_mercado);
+
+                                cliente.setNome(edtDialogNome.getText().toString());
+                                cliente.setNomeMercado(edtDialogMercado.getText().toString());
+
+                                //Verificar se os campos estão preenchidos
+                                if (!cliente.getNome().equals("") && !cliente.getNomeMercado().equals("")) {
+
+
+                                    mAdapter.updateList(cliente);
+
+                                } else { //Campos não preenchidos
+                                    Toast.makeText(getActivity(),
+                                            "Preencha todos os campos para adicionar um cliente",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("Cancelar", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        return view;
+    }
+    private void setupRecycler() {
+
+        // Configurando o gerenciador de layout para ser uma lista.
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // Adiciona o adapter que irá anexar os objetos à lista.
+        // Está sendo criado com lista vazia, pois será preenchida posteriormente.
+        mAdapter = new RecyclerClienteAdapter(new ArrayList<>(), getContext());
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Configurando um dividr entre linhas, para uma melhor visualização.
+        mRecyclerView.addItemDecoration(
+                new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+    }
     }
 
-}
+
