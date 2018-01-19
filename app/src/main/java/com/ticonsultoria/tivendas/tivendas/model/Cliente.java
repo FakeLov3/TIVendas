06e600e10f4e15bac441696c6ac4014d0f41b117
Helder Paiva <helder.ead@gmail.com>
2018-01-19 11:54:45 -0300

    clienteDAO criado e classe Cliente mudada falta apenas colocar a ação no onclik para salvar no banco

--- app/src/main/java/com/ticonsultoria/tivendas/tivendas/model/Cliente.java
+++ app/src/main/java/com/ticonsultoria/tivendas/tivendas/model/Cliente.java
@@ -6,11 +6,34 @@ package com.ticonsultoria.tivendas.tivendas.model;
 
 public class Cliente {
 
+    private int id_cliente;
     private String nome;
     private String cpf;
     private String nomeMercado;
+    private boolean ativo;
 
-    public Cliente() {
+    public Cliente(int id_cliente, String nome, String cpf, String nomeMercado, boolean ativo) {
+        this.id_cliente = id_cliente;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.nomeMercado = nomeMercado;
+        this.ativo = ativo;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
     }
 
     public String getNome() {
