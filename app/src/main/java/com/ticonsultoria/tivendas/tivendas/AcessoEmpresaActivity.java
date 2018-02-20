c89eda75009693574ee0098e52e358705f5269fa
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2018-02-20 09:57:48 -0300

    tela de acesso

--- app/src/main/java/com/ticonsultoria/tivendas/tivendas/AcessoEmpresaActivity.java
+++ app/src/main/java/com/ticonsultoria/tivendas/tivendas/AcessoEmpresaActivity.java
@@ -1,6 +1,7 @@
 package com.ticonsultoria.tivendas.tivendas;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 import com.ticonsultoria.tivendas.tivendas.BD.EmpresaDAO;
 import com.ticonsultoria.tivendas.tivendas.model.Empresa;
 import com.ticonsultoria.tivendas.tivendas.model.EmpresaAPI;
+import com.ticonsultoria.tivendas.tivendas.model.Usuario;
 
 import java.util.List;
 
@@ -24,13 +26,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AcessoEmpresaActivity extends AppCompatActivity {
 
+    private EmpresaDAO empresaDao;
+    private SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acesso_empresa);
+
+        verificarEmpresa();
         
-        final EmpresaDAO empresaDao = new EmpresaDAO(this);
-        final SharedPreferences[] sharedPreferences = new SharedPreferences[1];
+        empresaDao = new EmpresaDAO(this);
+        sharedPreferences = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
 
         Button botao = findViewById(R.id.button);
         final EditText editChave = findViewById(R.id.editText);
@@ -53,10 +60,7 @@ public class AcessoEmpresaActivity extends AppCompatActivity {
                     public void onResponse(Call<List<Empresa>> call, Response<List<Empresa>> response) {
                         if (response.body().size() > 0) {
                             Empresa empresa = response.body().get(0);
-                            empresaDao.salvar(empresa);
-
-
-
+                            acessar(empresa);
                         } else {
                             Toast.makeText(AcessoEmpresaActivity.this, "Chave de identificação incorreta", Toast.LENGTH_SHORT).show();
                         }
@@ -70,4 +74,25 @@ public class AcessoEmpresaActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void verificarEmpresa() {
+        String chaveEmpresa = sharedPreferences.getString("chave_empresa","");
+        if (!chaveEmpresa.equals("")) {
+            Intent intent = new Intent(AcessoEmpresaActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public void acessar(Empresa empresa){
+        empresaDao.salvar(empresa);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("chave_empresa", empresa.getEmp_chave());
+        editor.putInt("id_empresa", empresa.getId());
+        editor.commit();
+        Intent intent = new Intent(AcessoEmpresaActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
