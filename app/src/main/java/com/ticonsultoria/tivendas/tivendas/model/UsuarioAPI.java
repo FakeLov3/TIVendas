ded3a0960c4b63f3e6a3011a88993ddef6047dbd
Helder Paiva <helder.ead@gmail.com>
2018-02-20 11:23:10 -0300

    ClienteAPI, UsuarioAPI criados

---
+++ app/src/main/java/com/ticonsultoria/tivendas/tivendas/model/UsuarioAPI.java
@@ -0,0 +1,18 @@
+package com.ticonsultoria.tivendas.tivendas.model;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Helder on 20/02/2018.
+ */
+
+public interface UsuarioAPI {
+
+    public static final String BASE_URL = "http://cadizmob.kinghost.net/api/estagio";
+
+    @GET("usuarios")
+    Call<List<Usuario>> getListaUsuarios();
+}
